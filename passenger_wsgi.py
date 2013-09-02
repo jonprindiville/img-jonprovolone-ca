f23e0009fe9e314400b58430946423ea40384cd6
Jon Prindiville <jon.prindiville@gmail.com>
2013-09-02 11:56:37 -0700

    Fixed importing of jonprovolone into passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,15 @@
-# From: http://wiki.dreamhost.com/Passenger_WSGI
+# Based on: http://wiki.dreamhost.com/Passenger_WSGI
 
-import os, sys, jonprovolone
+import os, sys
 
-INTERP = "/path/to/interpreter"
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+# append current dir to module path
+sys.path.append(os.getcwd())
 
+# exec virtualenv interpreter if needed
+INTERP = "/home/jprind/img.jonprovolone.ca/ve/bin/python"
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+# this module is in the same directory as passenger_wsgi
+import jonprovolone
 application = jonprovolone.site.app
