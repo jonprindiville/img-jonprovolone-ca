fd0c8afe13da96b452bee1ba087e93e588cd2b11
Jon Prindiville <jon.prindiville@gmail.com>
2013-09-12 02:04:31 -0400

    Updates navigation through JS image loading

--- assets/image-loader.js
+++ assets/image-loader.js
@@ -1,7 +1,6 @@
-/* Load more images into the current HTML document
- *
- * TODO: update nav links as more images get loaded into the page?
- */
+/**********************************************************************
+ * Load more images into the current HTML document (depends on jQuery)*
+ *********************************************************************/
 
 // skipped and loaded are expected to have been inserted into the HTML
 // doc that this script is loaded from
@@ -26,8 +25,26 @@ function image_loader() {
             $('#more-images').replaceWith(data);
             attach_handler();
             new_skip = new_skip + loaded;
+
+            // Update the navigation at the top of the page -- if somebody
+            // hits that link the next page that is loaded should skip over
+            // images that we loaded here with our javascript.
+            $('#next-link').attr('href', '/n/' + loaded + '/s/' + new_skip );
         }
     );
+}
+
+/* Once there are no more images to load we use the following for the
+ * #more-images click handler. Toss some snark at the user before dropping
+ * #more-images. */
+current_warning = 0
+warning_fadeout = ['no more', 'that\'s all', '*sigh*', 'take a hint']
+function eventually_disappear() {
+    if (current_warning < warning_fadeout.length) {
+        $('#more-images p.msg').text(warning_fadeout[current_warning++]);
+    } else {
+        $('#more-images').fadeOut(1000);
+    }
 }
 
 /* Called on the document becoming ready and after loading new images */
@@ -47,9 +64,20 @@ function attach_handler() {
         // We've used the classes [more, no-more] to indicate if there are more
         // images to retrieve after the ones we just got
         if (more.hasClass('more')) {
-            more.attr('onclick', 'image_loader();');
+            more.click(image_loader);
         } if (more.hasClass('no-more')) {
-            more.attr('onclick', '$("#more-images").fadeOut(1000);');
+            // If the user continues to click, give a few "warnings" before
+            // disappearing the 'more' element
+            more.click(eventually_disappear);
+
+            // If we've got no more images we can lose the 'next' nav item
+            // (and maybe the whole nav element)
+            $('#next-link').fadeOut(1000, function() {
+                $('#next-link').remove();
+                if ($('header .nav').children().length == 0) {
+                    $('header .nav').remove();
+                }
+            });
         }
     }
 }
